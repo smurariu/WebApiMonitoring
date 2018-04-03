c30b158d04d1f3787fc5759bc0e6b15cbc65d207
Serban Murariu <Serban.Murariu@sundiogroup.com>
2018-04-03 11:36:32 +0200

    Added the machine name to the HealthCheck struct.

--- HealthCheck.cs
+++ HealthCheck.cs
@@ -6,13 +6,15 @@ namespace WebApiService
         public bool IsDown { get; }
         public int ResponseTimeMilliseconds { get; }
         public bool IsCritical { get; }
+        public string MachineName { get; set; }
 
-        public HealthCheck(string dependencyName, bool isDown, int responseTimeMilliseconds, bool isCritical = true)
+        public HealthCheck(string dependencyName, bool isDown, int responseTimeMilliseconds, bool isCritical = true, string machineName = null)
         {
             DependencyName = dependencyName;
             IsDown = isDown;
             ResponseTimeMilliseconds = responseTimeMilliseconds;
             IsCritical = isCritical;
+            MachineName = machineName ?? System.Environment.MachineName;
         }
     }
 }
